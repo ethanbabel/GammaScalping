a10b3c0a20ad6880d4da96551e1471a884a451ab
ebabel <babelethan@gmail.com>
2024-10-15 02:53:00 +0000

    high frequency expirementation, GARCH class

--- GARCH.py
+++ GARCH.py
@@ -1,22 +1,32 @@
 import yfinance as yf
 from datetime import datetime, timedelta
 import pandas as pd
-import matplotlib.pyplot as plt
 from arch import arch_model
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 import numpy as np
 
 class GARCH:
-    def __init__(self, ticker, start_date, end_date):
-        self.ticker = yf.Ticker(ticker)
-        self.start_date = start_date
-        self.end_date = end_date
-        self.returns = None
-        
-    def get_returns(self):
-        data = self.ticker.history(start=self.start_date, end=self.end_date)
-        self.returns = data['Close'].pct_change().dropna()
-        return self.returns
+    def __init__(self, ticker):
+        self.ticker = ticker
+        self.model = None
+        self.model_fit = None
     
-    def get_actual_volatility(self, returns):
-        return np.sqrt(returns.rolling(window=21).var())
+    def train(self, training_data, p, q):
+        self.model = arch_model(training_data, p=p, q=q)
+        self.model_fit = self.model.fit(disp='off')
+
+    def predict(self):
+        pred = self.model_fit.forecast(horizon=7)
+        todays_date = datetime.today().date()
+        future_dates = [todays_date + timedelta(days=i) for i in range(1,8)]
+        pred = pd.Series(np.sqrt(pred.variance.values[-1,:]), index=future_dates)
+        return pred
+
+if __name__ == "__main__":
+    spy = yf.Ticker("SPY")
+    data = 100 * spy.history(period="1y").Close.pct_change().dropna()
+    garch = GARCH(spy)
+    garch.train(data, 2, 2)
+    pred = garch.predict()
+    print(pred)
+    
+    
